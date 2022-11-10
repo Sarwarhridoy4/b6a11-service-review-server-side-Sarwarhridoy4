f5d8d371cc83b0813355666979fb80bfcfec6ced
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-11-10 17:38:07 +0600

    code for adding servicees done

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
-console.log(process.env.DB_USERNAME);
-console.log(process.env.DB_PASSWORD);
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -32,24 +30,32 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const userCollection = client.db("Weblogic").collection("Services");
+    // geting data
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = userCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
     });
+    // posting data to add more serrvice
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      console.log(service);
+      const result = await userCollection.insertOne(service)
+      console.log(result);
+    });
+    // getting data for home
     app.get("/services/home", async (req, res) => {
       const query = {};
       const cursor = userCollection.find(query).limit(3);
       const services = await cursor.toArray();
       res.send(services);
     });
-
+// getting data for showing service detail
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const service = await userCollection.findOne(query);
-
       res.send(service);
     });
   } finally {
